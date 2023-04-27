5c31b31abd1c2162685d4bf2c8c427086779c8a0
Shardul Kurdukar <24shardul@gmail.com>
2023-04-27 09:42:48 +0530

    added mandelbrot and julia

---
+++ src/components/ConfigPanel.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+import { Form, Card } from "react-bootstrap";
+
+const ConfigPanel = ({ config, setConfig }) => {
+  return (
+    //use react bootstrap to create controlled form for all config options
+    <Card className="z-3 position-absolute p-1 m-2">
+      <Card.Body>
+        <Form>
+          <Form.Group controlId="formBasicRange">
+            <Form.Label>Color Palette</Form.Label>
+            <Form.Control
+              type="range"
+              min="0"
+              max="6"
+              step="1"
+              value={config.colorPaletteIndex}
+              onChange={(e) =>
+                setConfig({ ...config, colorPaletteIndex: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicRange">
+            <Form.Label>Julia Set C Value X</Form.Label>
+            <Form.Control
+              type="range"
+              min="-1"
+              max="1"
+              step="0.001"
+              value={config.julia_c[0]}
+              onChange={(e) =>
+                setConfig({
+                  ...config,
+                  julia_c: [parseFloat(e.target.value), config.julia_c[1]],
+                })
+              }
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicRange">
+            <Form.Label>Julia Set C Value Y</Form.Label>
+            <Form.Control
+              type="range"
+              min="-1"
+              max="1"
+              step="0.001"
+              value={config.julia_c[1]}
+              onChange={(e) =>
+                setConfig({
+                  ...config,
+                  julia_c: [config.julia_c[0], parseFloat(e.target.value)],
+                })
+              }
+            />
+          </Form.Group>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ConfigPanel;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
